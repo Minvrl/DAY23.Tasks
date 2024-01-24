073cb2bb8a1c8b8135ea854a9547ffbe41513cb6
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-01-24 15:07:10 +0400

    Add files via upload

---
+++ DAY23.Tasks/DAY23.Tasks/ExtentionMethods.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY23.Tasks
+{
+    internal static class ExtentionMethods
+    {
+        public static bool IsOdd(this int num)
+        {
+            return num % 2 != 0;
+        }
+
+        public static bool IsEven(this int num)
+        {
+            return num % 2 == 0;
+        }
+
+        public static bool IsContaintsDigit(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsDigit(str[i])) return true;
+            }
+            return false;
+        }
+
+        public static string ToCapitalize(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            char firstLetter = char.ToUpper(str[0]);
+            return firstLetter + str.Substring(1).ToLower();
+        }
+
+
+        public static int[] GetValueIndexes(this string str, char chr)
+        {
+            int[] indexes = new int[0];    
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == chr)
+                {
+                    Array.Resize(ref indexes, indexes.Length + 1);
+                    indexes[indexes.Length - 1] = i;
+                }
+            }
+
+            return indexes;
+        }
+
+        public static string GetFirstSentence(this string text)
+        {
+            int endIndex = text.IndexOf('.');
+            int startIndex = 0;
+            int length = endIndex - startIndex;
+
+            return text.Substring(startIndex, length);
+        }
+
+        public static string GetSecondWord(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+
+            int startIndex = str.IndexOf(' ');
+
+            if (startIndex == -1)
+            {
+                return null;  
+            }
+
+            startIndex++;  
+
+            int endIndex = str.IndexOf(' ', startIndex);
+
+            if (endIndex == -1)
+            {
+                return str.Substring(startIndex);  
+            }
+
+            int length = endIndex - startIndex;
+            return str.Substring(startIndex, length);
+        }
+
+
+        public static string FixSpacing(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                return str;  
+            }
+
+            StringBuilder result = new StringBuilder();
+            bool isSpace = false;
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char c = str[i];
+
+                if (char.IsWhiteSpace(c))
+                {
+                    isSpace = true;
+                }
+                else
+                {
+                    if (isSpace)
+                    {
+                        result.Append(' ');  
+                        isSpace = false;
+                    }
+
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString().Trim();  
+        }
+
+    }
+}
---
+++ DAY23.Tasks/DAY23.Tasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using DAY23.Tasks;
+
+int numOdd = 6;
+Console.WriteLine(numOdd.IsOdd());
+
+int numEven = 4;
+Console.WriteLine(numEven.IsEven());
+
+string str3 = "salam 3 necesiz";
+Console.WriteLine(str3.IsContaintsDigit());
+
+
+string str4 = "caAPiTalIzIng";
+Console.WriteLine(str4.ToCapitalize());
+
+
+string text = "salaam";
+char chr = 'a';
+int[] charIndexes = text.GetValueIndexes(chr);
+
+for (int i = 0; i < charIndexes.Length; i++)
+{
+    Console.WriteLine($"[{charIndexes[i]}]");
+}
+
+
+string metn = "Men yagisi sevirem. Amma qari daha cox sevirem. Qış en gozel fesildir.";
+Console.WriteLine(metn.GetFirstSentence());
+
+
+//Bosluqlar nezere alinmadan
+string str = "salam necesiz doslar";
+
+Console.WriteLine(str.GetSecondWord());
+
+
+//Bosluqlar nezere alinarsa
+string str2 = "  A      snow  storm  ";
+string fixedString = str2.FixSpacing();
+Console.WriteLine(fixedString.GetSecondWord()); 
